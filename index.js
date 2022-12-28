55805d6d8d8bbd4654cda58a6f81bec10a5593fb
thanhhuy0611 <thanhhuy0611@gmail.com>
2022-12-28 16:27:23 +0700

    test jenkins pipeline

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const http = require("http");
 
 const server = http.createServer((req, res) => {
-  res.end("Hello Jenkins\n")
+  res.end("Hello Jenkins pipeline - Jenkinsfile\n")
 });
 
 server.listen(3000, () => {
